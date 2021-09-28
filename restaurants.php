be384cfe6ef9d77e678e6c986f4c5540fc4be5fb
MatthieuChabot <chbt.matthieu@gmail.com>
2021-09-28 22:03:03 +0200

    Ajout Logo, Navigation via burger mais mal faite, div pour articles

--- restaurants.php
+++ restaurants.php
@@ -1,10 +1,48 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
 <head>
     <title>Restaurants - Les Lions</title>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width"/>
     <link href="restaurants.css" rel="stylesheet">
 </head>
 <body>
+<div class="header">
+<div class="menu"><a href="#nav">Menu</a></div>
+<nav id="nav">
+<ul>
+        <li><a href="restaurants.php">Restaurants</a></li>
+        <li><a href="#">Bars</a></li>
+        <li><a href="#">Sports</a></li>
+        <li><a href="#">Monuments</a></li>
+        <li><a href="#">Fermer le menu</a></li>
+    </ul>
+</nav>
+<img class="image" src="https://www.lyon.fr/sites/lyonfr/files/content/2017-07/VDL-logo.jpg">
+</div>
+<hr class="ligne-menu">
+<h1>Restaurants à Lyon</h1>
+<section class="section-article">
+<div class="article">
+    <h2>Paul Bocuse</h2>
+    <img class="image-article" src=https://images.unsplash.com/photo-1602087594298-706ccc894bfd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1632&q=80>
+    <p>Une étoile</p>
+</div>
+<div class="article">
+    <h2>Georges Blanc</h2>
+    <img class="image-article" src=https://images.unsplash.com/photo-1602087594298-706ccc894bfd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1632&q=80>
+    <p>Une étoile</p>
+</div>
+<div class="article">
+    <h2>Jean-Paul Rouve</h2>
+    <img class="image-article" src=https://images.unsplash.com/photo-1602087594298-706ccc894bfd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1632&q=80>
+    <p>Une étoile</p>
+</div>
+<div class="article">
+    <h2>Michel Drucker</h2>
+    <img class="image-article" src=https://images.unsplash.com/photo-1602087594298-706ccc894bfd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1632&q=80>
+    <p>Une étoile</p>
+</div>
+</section>
 </body>
 </html>
