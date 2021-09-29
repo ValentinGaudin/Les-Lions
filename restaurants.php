e6559a5d9e0b233c1d8f295911b6cc30f581a886
MatthieuChabot <chbt.matthieu@gmail.com>
2021-09-29 10:14:15 +0200

    Erasement of header and css of button up start

--- restaurants.php
+++ restaurants.php
@@ -7,20 +7,6 @@
     <link href="restaurants.css" rel="stylesheet">
 </head>
 <body>
-<div class="header">
-<div class="menu"><a href="#nav">Menu</a></div>
-<nav id="nav">
-<ul>
-        <li><a href="restaurants.php">Restaurants</a></li>
-        <li><a href="#">Bars</a></li>
-        <li><a href="#">Sports</a></li>
-        <li><a href="#">Monuments</a></li>
-        <li><a href="#">Fermer le menu</a></li>
-    </ul>
-</nav>
-<img class="image" src="https://www.lyon.fr/sites/lyonfr/files/content/2017-07/VDL-logo.jpg">
-</div>
-<hr class="ligne-menu">
 <h1>Restaurants à Lyon</h1>
 <section class="section-article">
 <div class="article">
@@ -41,7 +27,8 @@
 <div class="article">
     <h2>Michel Drucker</h2>
     <img class="image-article" src=https://images.unsplash.com/photo-1602087594298-706ccc894bfd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1632&q=80>
-    <p>Une étoile</p>
+    <p>Lorem ipsum 20</p>
+    <p>note</p>
 </div>
 </section>
 </body>
