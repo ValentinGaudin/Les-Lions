99189d1e94ce14ed06fb36ee0775df2d34fe9eef
ValentinGaudin <valentingaudin@gmail.com>
2021-09-29 11:43:14 +0200

    Index of the main

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Lions</title>
+        <title>The Lions</title>
         <link rel="stylesheet" type="text/css" href="css/style.css">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/indexStyle.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins:wght@100&display=swap" rel="stylesheet">
@@ -22,7 +22,65 @@
         <?php include("_header.php"); ?>
         <script src="script.js"></script>
 
+        <!-- Article dans l'index -->
 
+        <section class="lastArticle container" >
+                <div class="topArticleL containerA">
+                        <article>
+                                <h4>Bocuse</h4>
+                                <a href="#"> <img src="" alt="Le restaurant de Paul Bocuse" ></a>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum unde repudiandae nisi fugiat iure, quam alias quibusdam! Quae, distinctio nostrum?
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro cupiditate fuga itaque culpa quasi accusamus in alias voluptatum, ex molestiae quidem iusto quisquam doloremque obcaecati similique voluptates quod, quo voluptate!
+                                </p>
+                        </article>
+                </div>
+
+                <div class="topArticleR containerB">
+                        <article>
+                        <h4>Le parc de la tête d'or</h4>
+                                <a href="#"> <img src="" alt="Le parc de la tête d'or" ></a>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum unde repudiandae nisi fugiat iure, quam alias quibusdam! Quae, distinctio nostrum?
+                                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem ipsum iusto, itaque aliquid, eligendi qui quasi earum minus, rem reiciendis mollitia minima corrupti sint harum? Porro, nemo. Delectus, at nostrum!
+                                </p>
+                        </article>
+                </div>
+
+                <div class="articleL containerC">
+                        <article>
+                        <h4>Le musée de confluence</h4>
+                                <a href="#"> <img src="" alt="Le musée de Confluence" ></a>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum unde repudiandae nisi fugiat iure, quam alias quibusdam! Quae, distinctio nostrum?
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt porro architecto nemo reprehenderit quibusdam praesentium, ipsa, quaerat, reiciendis vitae sit maiores adipisci vel veritatis libero. Quis tenetur officiis tempora doloribus.
+                                </p>
+                        </article>
+                        
+                </div>
+
+                <div class="articleR containerD">
+                        <article><h4>Le soda Bar</h4>
+                                <a href="#"> <img src="" alt="Restaurant de cokctails, Soda Bar" ></a>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum unde repudiandae nisi fugiat iure, quam alias quibusdam! Quae, distinctio nostrum?
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione reiciendis sapiente, numquam tempora accusantium labore error, ea praesentium, delectus aliquid autem quos corrupti illum corporis sunt. Dolorem nihil eveniet doloremque.
+                                </p>
+                        </article>
+                </div>
+                <div class="articleL containerE">
+                        <article><h4>Place Bellecour</h4>
+                                <a href="#"> <img src="" alt="La plus grande place de Lyon, Bellecour" ></a>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum unde repudiandae nisi fugiat iure, quam alias quibusdam! Quae, distinctio nostrum?
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione reiciendis sapiente, numquam tempora accusantium labore error, ea praesentium, delectus aliquid autem quos corrupti illum corporis sunt. Dolorem nihil eveniet doloremque.
+                                </p>
+                        </article>
+                </div>
+                <div class="articleR containerF">
+                        <article><h4>Fourvière</h4>
+                                <a href="#"> <img src="" alt="Cathédrale notre Dame de Fourvière" ></a>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum unde repudiandae nisi fugiat iure, quam alias quibusdam! Quae, distinctio nostrum?
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione reiciendis sapiente, numquam tempora accusantium labore error, ea praesentium, delectus aliquid autem quos corrupti illum corporis sunt. Dolorem nihil eveniet doloremque.
+                                </p>
+                        </article>
+                </div>
+        </section>
 </body>
 
 </html>
