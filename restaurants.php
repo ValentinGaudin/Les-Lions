7cf302b404d0dee66cd6784068512cc487df5a5d
MatthieuChabot <chbt.matthieu@gmail.com>
2021-09-29 11:44:30 +0200

    Css of article's divs for theme pages

--- restaurants.php
+++ restaurants.php
@@ -12,22 +12,25 @@
 <div class="article">
     <h2>Paul Bocuse</h2>
     <img class="image-article" src=https://images.unsplash.com/photo-1602087594298-706ccc894bfd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1632&q=80>
+    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     <p>Une étoile</p>
 </div>
 <div class="article">
     <h2>Georges Blanc</h2>
     <img class="image-article" src=https://images.unsplash.com/photo-1602087594298-706ccc894bfd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1632&q=80>
+    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     <p>Une étoile</p>
 </div>
 <div class="article">
     <h2>Jean-Paul Rouve</h2>
     <img class="image-article" src=https://images.unsplash.com/photo-1602087594298-706ccc894bfd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1632&q=80>
+    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     <p>Une étoile</p>
 </div>
 <div class="article">
     <h2>Michel Drucker</h2>
     <img class="image-article" src=https://images.unsplash.com/photo-1602087594298-706ccc894bfd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1632&q=80>
-    <p>Lorem ipsum 20</p>
+    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     <p>note</p>
 </div>
 </section>
