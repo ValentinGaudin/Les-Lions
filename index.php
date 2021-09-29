5bb49c5cbd21caf9dadefc1620c897e7ddb5707e
ValentinGaudin <valentingaudin@gmail.com>
2021-09-29 10:22:12 +0200

    Modification of index css

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Lions</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins:wght@100&display=swap" rel="stylesheet">
@@ -21,7 +22,7 @@
         <?php include("_header.php"); ?>
         <script src="script.js"></script>
 
-        
+
 </body>
 
 </html>
