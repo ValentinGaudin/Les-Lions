0dc07b9c4d58930b08341437ca11470cc94cb72e
MatthieuChabot <chbt.matthieu@gmail.com>
2021-09-30 17:08:59 +0200

    Fixing img problems and titles

--- main/Article/article_default.php
+++ main/Article/article_default.php
@@ -22,9 +22,7 @@
 <?php include ("../header.php");?>
 <div class='article_details'>
 
-    <div class='article_titre'>
-        <h1>Titre de l'article</h1>
-    </div>
+        <h1 class="article_titre">Titre de l'article</h1>
 
     <div class='article_contenu'>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima porro perferendis sequi dicta deserunt, impedit ipsam quas libero. Optio perferendis molestias odit ipsam, quo natus. Laboriosam ratione dolor minima odio ullam repudiandae expedita voluptatibus perferendis eos, cum at, aliquid magnam quidem mollitia. Sit voluptatum eos, odit eligendi facere deleniti autem?</p>    
