c2051a6d6fa51b3e849b5dbc739d2117dec3ded6
MatthieuChabot <chbt.matthieu@gmail.com>
2021-10-02 12:47:58 +0200

    Adding the 4 articles pages and links in theme page

---
+++ public/Culture/Articles/fourviere.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>The Lions</title>
+
+        <link rel="stylesheet" type="text/css" href="/articleStyle.css">
+        <link rel="stylesheet" type="text/css" href="/header.css">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins:wght@100&display=swap" rel="stylesheet">
+        <?php
+        // set the default timezone to use.
+        date_default_timezone_set('UTC');
+        ?>
+</head>
+
+<body>
+<!-- Barre de navgiation & bannière -->
+<?php include ("../../header.php");?>
+
+<div class='article_details'>
+
+        <h1 class="article_titre">Titre de l'article</h1>
+
+    <div class='article_contenu'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima porro perferendis sequi dicta deserunt, impedit ipsam quas libero. Optio perferendis molestias odit ipsam, quo natus. Laboriosam ratione dolor minima odio ullam repudiandae expedita voluptatibus perferendis eos, cum at, aliquid magnam quidem mollitia. Sit voluptatum eos, odit eligendi facere deleniti autem?</p>    
+        <br>
+        
+    </div>
+
+    <div class='article_notation'>
+
+        <p>Vous avez aimez l'article ? Rajoutez lui une étoile !</p>
+        <img src="/images/red-star.png">
+    </div>
+
+</div>
+<div class="div-bouton-up"><div class="bouton-up"><a href="#top"><img id="bouton" src="/images/fleche-bouton-up.png" alt="up button"></a></div></div>
+
+</body>
--- public/Culture/culture.php
+++ public/Culture/culture.php
@@ -26,29 +26,29 @@
 
 <section class="sectionArticles">
 
-    <article class="positioningImage" >
-        <h2>Paul Bocuse</h2>
+    <article class="fourviere positioningImage" >
+        <h2><a href="Articles/fourviere.php">Basilique de Fourvière</a></h2>
             <img class="imageArticle" src=>
             <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             <p>Une étoile</p>
     </article>
 
-    <article>
-        <h2>Georges Blanc</h2>
+    <article class="mac positioningImage">
+        <h2><a href="Articles/mac.php">Musée d'Art Contemporain</a></h2>
+            <img class="imageArticle">
+            <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p>Une étoile</p>
+    </article>
+
+    <article class="miniature positioningImage">
+        <h2><a href="Articles/miniature.php">Musée des miniatures</a></h2>
             <img class="imageArticle" src=>
             <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             <p>Une étoile</p>
     </article>
 
-    <article>
-        <h2>Jean-Paul Rouve</h2>
-            <img class="imageArticle" src=>
-            <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <p>Une étoile</p>
-    </article>
-
-    <article>
-        <h2>Michel Drucker</h2>
+    <article class="confluence positioningImage">
+        <h2><a href="Articles/confluence.php">Musée des Confluences</a></h2>
             <img class="imageArticle" src=>
             <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             <p>note</p>
