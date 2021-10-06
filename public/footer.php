f031c4cafb82cbc2da002413efbee4b5e33b91ab
Manuela Quertier <manuelaquertier@gmail.com>
2021-10-06 14:08:19 +0200

    editing chatbot

--- public/footer.php
+++ public/footer.php
@@ -1,39 +1,49 @@
 
 <footer>
 <div class="Chatbot">
-        <div class="msg-header">Besoin d'aide ?</div>
-        <div class="">
+    <div class="msg-header">Besoin d'aide ?</div>
+    <div class="firstChoice">
                 <div class="Question">
                     <p>Que voulez vous faire à Lyon?</p>
                 </div>
-        </div>
         <div class="choices">
             <div class="button">
-                <button class="" onclick= "sportFunction()" type="button">Faire du sport</button>
+                <button id="sport" onclick= "sportFunction()" type="button">Faire du sport</button>
             </div>
             <div class="button">
-                <button class="" onclick= "cultureFunction()" type="button">Faire du tourisme/découvrir</button>
+                <button id="culture" onclick= "cultureFunction()" type="button">Faire du tourisme/découvrir</button>
             </div>
             <div class="button">
-                <button class="" onclick= "barFunction()" type="button">Aller boire un verre</button>
+                <button id="bar" onclick= "barFunction()" type="button">Aller boire un verre</button>
             </div>
             <div class="button">
-                <button class="" onclick= "restaurantFunction()" type="button">Trouver un restaurant</button>
+                <button id="restaurant" onclick= "restaurantFunction()" type="button">Trouver un restaurant</button>
             </div>
         </div>
     </div>
-    <script>
-        function sportFunction() {
-            location.href="Sports/sports.php";
-        }
-        function cultureFunction() {
-            location.href="Culture/culture.php";
-        }
-        function barFunction() {
-            location.href="Bars/bars.php";
-        }
-        function restaurantFunction() {
-            location.href="Restaurants/restaurants.php";
-}
-</script>
+    <div class="choicesSport">
+            <div class="Question">
+                <p>Comment souhaitez vous faire du Sport</p>
+            </div>
+            <div class="button">
+                <button id="interior" onclick= "interiorFunction()" type="button">En intérieur</button>
+            </div>
+            <div class="button">
+                <button class="exterior" onclick= "exteriorFunction()" type="button">A l'extérieur</button>
+            </div>
+    </div>
+
+    <div class="choicesCulture">
+            <div class="Question">
+                <p>Que souhaitez vous visiter?</p>
+            </div>
+            <div class="button">
+                <button id="interior" onclick= "interiorFunction()" type="button">En intérieur</button>
+            </div>
+            <div class="button">
+                <button class="exterior" onclick= "exteriorFunction()" type="button">A l'extérieur</button>
+            </div>
+    </div>
+
+    <script src="script/footer.js"></script>
 </footer>
